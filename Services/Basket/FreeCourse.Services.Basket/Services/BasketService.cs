7560ae39686041864b5d3efefaeca43a9ba1d0ed
SEFA ŞIHLAR <sihlarsefa7@gmail.com>
2023-09-29 10:53:00 +0300

    Basket service created and basketservice redisservice created and controller complated

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,50 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+using System.Text.Json;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService=redisService;
+        }
+
+        public async Task<ResponseDto<bool>> Delete(string userId)
+        {
+            var sataus = await _redisService.GetDb().KeyDeleteAsync(userId);
+            return sataus ? ResponseDto<bool>.Success(204) : ResponseDto<bool>.Fail("not found ", 404);
+
+        }
+
+        public async Task<ResponseDto<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userId);
+            if (string.IsNullOrEmpty(existBasket))
+            {
+                return ResponseDto<BasketDto>.Fail("Basket Not Found", 404);
+            }
+
+            return ResponseDto<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket),200);
+
+        }
+
+
+
+        public async Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+          
+                var serializedBasket = JsonSerializer.Serialize(basketDto);
+                var sataus  = await _redisService.GetDb().StringSetAsync(basketDto.UserId, serializedBasket);
+
+
+            return sataus ? ResponseDto<bool>.Success(204) : ResponseDto<bool>.Fail("Basket coluld not update or save", 500);
+               
+          
+          
+        }
+    }
+}
---
+++ Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<ResponseDto<BasketDto>> GetBasket(string userId);
+
+        Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
+
+        Task<ResponseDto<bool>> Delete(string userId);
+
+    }
+}
