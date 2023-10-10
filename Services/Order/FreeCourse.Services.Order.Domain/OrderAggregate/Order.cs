5ef5ee6d33677c99cb44b1b119797429995a5927
SEFA ŞIHLAR <sihlarsefa7@gmail.com>
2023-10-10 12:35:43 +0300

    Entity,IAggregateRoot,ValueObject,OrderAggregate operations created and complated

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,42 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; set; }
+        public Address Address { get; private set; }
+
+        public string BuyerId { get; set; }
+
+        private readonly List<OrderItem> _orderItems;
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId = buyerId;
+            Address = address;
+        }
+
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+
+            var existproduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existproduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal TotalPrice=>_orderItems.Sum(x => x.Price);
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,34 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem:Entity
+    {
+        public OrderItem(string productId, string name, string ımageUrl, decimal price)
+        {
+            ProductId=productId;
+            Name=name;
+            ImageUrl=ımageUrl;
+            Price=price;
+        }
+
+        public string ProductId { get;private set; }
+        public string Name { get; private set; }
+        public string ImageUrl { get; private set; }
+        public decimal Price { get; private set; }
+
+
+        public void UpdaterItem(string name, string imageUrl, decimal price)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            Price = price;
+       
+        }
+    }
+}
